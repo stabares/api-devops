a365474b569332c893eb0948dc0b7c6fc402ebb6
support <support@quind.io>
2022-05-05 00:06:09 -0500

    conexion bd mongo

--- src/main/java/com/amoelcodigo/crud/repository/TorreRepository.java
+++ src/main/java/com/amoelcodigo/crud/repository/TorreRepository.java
@@ -1,13 +1,13 @@
 package com.amoelcodigo.crud.repository;
 
 import com.amoelcodigo.crud.entity.Torre;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface TorreRepository  extends JpaRepository<Torre, Integer> {
+public interface TorreRepository  extends MongoRepository<Torre, Integer> {
         // Con @Repository le indico los metodos principales select
 
     //Convención sobre convicción
